103144a6e6f7015186ecbe509580493b080df2d6
Simon Walker <s.r.walker101@googlemail.com>
2014-04-21 12:55:10 +0100

    Adds basic (non-functional) setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from glob import glob
+import re
+
+package_name = 'delegator'
+version_str = re.search(r'^__version__\s+=\s+[\'"]([\d.]+)[\'"]',
+        open('%s/version.py' % (package_name, )).read(),
+        re.M).group(1)
+
+setup(name=package_name,
+        version=version_str,
+        description='Simple delegator class',
+        author='Simon Walker',
+        author_email='s.r.walker101@googlemail.com',
+        url='http://github.com/mindriot101/python-delegator',
+        packages=['delegator', ],
+        long_description=open('README.markdown').read(),
+        )
--- testing/test_python_delegator.py
+++ testing/test_python_delegator.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.append('.')
 from delegator import Delegator
 
 class Delegatee(object):
